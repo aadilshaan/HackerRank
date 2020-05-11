39f7175b683cdb7dd623e3abdcc07845261b32b2
aadilshaan <52848868+aadilshaan@users.noreply.github.com>
2020-05-12 04:31:06 +0530

    Add files via upload

---
+++ challenge03.py
@@ -0,0 +1,12 @@
+# Arithmetic Operators
+
+num1 = input()
+num1 = int(num1)
+num2 = input()
+num2 = int(num2)
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+print(add)
+print(sub)
+print(mult)
