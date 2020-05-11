ea4b1f8db7210d793d294f7f1b90ce69b7163ab6
aadilshaan <52848868+aadilshaan@users.noreply.github.com>
2020-05-12 04:01:54 +0530

    Add files via upload

---
+++ challenge02.py
@@ -0,0 +1,12 @@
+# Pyhton If-Else
+
+num = input()
+num = int(num)
+if num%2 == 1:
+    print('Weird')
+elif num <= 5:
+    print('Not Weird')
+elif num <= 20:
+    print('Weird')
+else:
+    print('Not Weird')
