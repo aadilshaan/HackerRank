e82b142db20da3424f0b3780398a5f202c05a741
aadilshaan <52848868+aadilshaan@users.noreply.github.com>
2020-05-13 16:20:53 +0530

    Add files via upload

---
+++ Day 01.py
@@ -0,0 +1,13 @@
+# Data Types
+
+i = 4
+d = 4.0
+s = 'HackerRank ' 
+int_no = int(input())
+float_no = float(input())
+string = input()
+i_i = int_no + i
+f_d = float_no + d
+print(int(i_i))
+print(f_d)
+print(s + string)
